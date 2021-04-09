8c6e8e43f44780aa017868feb6117a4a2d40de7d
K54088 <rogers.kao@gmail.com>
2021-04-08 22:02:12 -0400

    Web05 first push

---
+++ src/test/java/kao/ij1/web05/Web05ApplicationTests.java
@@ -0,0 +1,13 @@
+package kao.ij1.web05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
